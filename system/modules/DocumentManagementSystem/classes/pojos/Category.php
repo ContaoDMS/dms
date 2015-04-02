bd24af0396238ccde5ee1c67336eb12164eb499e
Cliff Parnitzky <github@cliff-parnitzky.de>
2015-04-02 17:44:42 +0200

    Do not allow uploading in categories which have not allowed file types defined (closes #28)

--- system/modules/DocumentManagementSystem/classes/pojos/Category.php
+++ system/modules/DocumentManagementSystem/classes/pojos/Category.php
@@ -470,7 +470,7 @@ class Category
 	 */
 	public function isUploadableForCurrentMember()
 	{
-		return $this->isAccessibleForCurrentMember(\AccessRight::UPLOAD);
+		return $this->isAccessibleForCurrentMember(\AccessRight::UPLOAD) && count($this->getAllowedFileTypes()) > 0;
 	}
 	
 	/**
