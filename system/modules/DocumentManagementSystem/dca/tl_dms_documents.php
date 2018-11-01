6f48d1134163bbd3b378f127772dc757d0a9a9ea
Cliff Parnitzky <github@cliff-parnitzky.de>
2018-11-01 23:35:27 +0100

    Adjust DCA to use correct toggle icon mechanism.

--- system/modules/DocumentManagementSystem/dca/tl_dms_documents.php
+++ system/modules/DocumentManagementSystem/dca/tl_dms_documents.php
@@ -363,7 +363,7 @@ class tl_dms_documents extends \Backend
 			$icon = 'invisible.gif';
 		}
 
-		return '<a href="'.$this->addToUrl($href).'" title="'.specialchars($title).'"'.$attributes.'>'.$this->generateImage($icon, $label).'</a> ';
+		return '<a href="'.$this->addToUrl($href).'" title="'.specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label, 'data-state="' . ($row['published'] ? 1 : 0) . '"').'</a> ';
 	}
 
 	/**
