9d3a46505fbfd92ed23632cfcab473dd2e45233d
Cliff Parnitzky <github@cliff-parnitzky.de>
2019-12-18 00:56:26 +0100

    Reverted everything of #52

--- system/modules/DocumentManagementSystem/modules/ModuleDmsManagement.php
+++ system/modules/DocumentManagementSystem/modules/ModuleDmsManagement.php
@@ -324,7 +324,7 @@ class ModuleDmsManagement extends \Module
     else
     {
       // move the uploaded file to dms temp dir
-      //move_uploaded_file($_FILES['dmsFile']['tmp_name'], \DmsConfig::getTempDirectory(true) . $strFileNameUnversioned);
+      move_uploaded_file($_FILES['dmsFile']['tmp_name'], \DmsConfig::getTempDirectory(true) . $strFileNameUnversioned);
       
       // load possible documents for file name
       $params->loadCategory = true; // need the category of existing documents
@@ -452,7 +452,7 @@ class ModuleDmsManagement extends \Module
       $arrMessages['errors'][] = $GLOBALS['TL_LANG']['DMS']['ERR']['upload_document_not_allowed'];
       $blnShowStart = true;
     }
-    else if (!file_exists(\DmsConfig::getTempDirectory(true) . $tempFileNameCleaned))
+    else if (!file_exists(TL_ROOT . '/' . \DmsConfig::getTempDirectory(true) . $tempFileNameCleaned))
     {
       $arrMessages['errors'][] = $GLOBALS['TL_LANG']['DMS']['ERR']['upload_temp_file_not_found'];
       $blnShowStart = false;
@@ -521,13 +521,8 @@ class ModuleDmsManagement extends \Module
         $documentVersion = \Document::buildVersionForFileName($documentVersionMajor, $documentVersionMinor, $documentVersionPatch);
         $fileFileNameVersioned = \Document::buildFileNameVersioned($arrTempFileParts['fileName'], $documentVersion, $arrTempFileParts['fileType']);
         
-        // move the uploaded file to dms temp dir
-        move_uploaded_file($_FILES['dmsFile']['tmp_name'], \DmsConfig::getBaseDirectory(true) . $tempFileNameCleaned);
-        
         // move the temp file to dms dir and append version
-        rename(\DmsConfig::getBaseDirectory(true) . $tempFileNameCleaned, \DmsConfig::getDocumentFilePath($fileFileNameVersioned));
-        //move_uploaded_file(\DmsConfig::getTempDirectory(true) . $tempFileNameCleaned, \DmsConfig::getDocumentFilePath($fileFileNameVersioned));
-        //print_r($fileFileNameVersioned);
+        rename(\DmsConfig::getTempDirectory(true) . $tempFileNameCleaned, \DmsConfig::getDocumentFilePath($fileFileNameVersioned));
         \Dbafs::addResource(\DmsConfig::getDocumentFilePath($fileFileNameVersioned));
         
         // store document
